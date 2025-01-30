58a4d649438121710c3378fba3e8e5fbbc197ae9
Claus Reinhold <claus.reinhold@gmail.com>
2025-01-30 11:37:34 +0100

    more stuff

--- components/message.tsx
+++ components/message.tsx
@@ -27,17 +27,19 @@ export const Message = ({
       initial={{ y: 5, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
     >
-      <div className="size-[24px] border rounded-sm p-1 flex flex-col justify-center items-center shrink-0 text-zinc-500">
+      <>
         {role === "assistant" ? (
           <BotIcon />
         ) : isLoading ? (
-          <>
+          <div className="size-[24px] border rounded-sm p-1 flex flex-col justify-center items-center shrink-0 text-zinc-500">
             <Loader2 className="animate-spin size-5 text-foreground" />
-          </>
+          </div>
         ) : (
-          <UserIcon />
+          <div className="size-[24px] border rounded-sm p-1 flex flex-col justify-center items-center shrink-0 text-zinc-500">
+            <UserIcon />
+          </div>
         )}
-      </div>
+      </>
       <div className="flex flex-col gap-2 w-full">
         {content && typeof content === "string" && (
           <div className="text-zinc-800 dark:text-zinc-300 flex flex-col gap-4">
